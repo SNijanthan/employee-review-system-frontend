0caf8f42a5ab5ac1bdcbdf78a585f086d901b73a
SNijanthan <nijanthan378@gmail.com>
2025-04-11 22:38:45 +0530

    Changes made for deployment

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,4 @@
 export const BASE_URL =
-  location.hostname === "localhost" ? "http://localhost:7777" : "/api";
+  location.hostname === "localhost"
+    ? "http://localhost:7000"
+    : "https://employee-review-system-backend-dc9y.onrender.com";
